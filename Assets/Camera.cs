f9e13c82b8eaeb38115e344c6495fba450dc88b3
KirillKom <kirill-e-komarov@mail.ru>
2024-04-11 14:41:19 +0500

    c-3

---
+++ Assets/Camera.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    [SerializeField] private float scrollSpeed;
+    [SerializeField] private float vertScrollRange;
+    [SerializeField] private float horScrollRange;
+}
