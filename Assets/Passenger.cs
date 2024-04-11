9a4c7261ed17c8ab2a4bbbe11a5be0ef3b0d1ee1
KirillKom <kirill-e-komarov@mail.ru>
2024-04-11 14:26:14 +0500

    c-1

---
+++ Assets/Passenger.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Passenger : Human
+{
+    [SerializeField] private string flight;
+    [SerializeField] private bool documents;
+    [SerializeField] private int mood;
+    [SerializeField] private int folly;
+
+    //если mood < 50 - идёт в киоск или кафе
+    //folly отвечает за шанс совершить нарушение
+}
